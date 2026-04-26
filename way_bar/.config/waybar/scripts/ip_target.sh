9d4c7c843369c83733667ece154b09711200b086
HyprArch <kirito0160218@gmail.com>
2026-04-25 21:58:51 -0600

    update 2026-04-25 21:53

--- way_bar/.config/waybar/scripts/ip_target.sh
+++ way_bar/.config/waybar/scripts/ip_target.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+path_to_file="/home/suromih/.config/waybar/scripts/target.txt"
+
 function do_all(){
-  if [ $(wc -c /home/suromih/.config/waybar/scripts/target.txt | awk '{print $1}') -gt 1 ]; then
-      target=$(awk '{print}' target.txt)
+  if [ $(wc -c $path_to_file | awk '{print $1}') -gt 1 ]; then
+      target=$(awk '{print}' $path_to_file)
       echo "{\"text\": \" $target\", \"class\": \"connected\"}"
   else
       echo "{\"text\": \" Not target\", \"class\": \"disconnected\"}"
@@ -12,6 +14,6 @@ function do_all(){
 if [[ -f ".config/waybar/scripts/target.txt" ]]; then
   do_all
 else
-  touch /home/suromih/.config/waybar/scripts/target.txt
+  touch $path_to_file
   do_all
 fi
